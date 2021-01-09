3d4bef998fbdc2d7c49178066b294eb39c1f1d10
sasagawa888 <ken-s@tkcnf.or.jp>
2021-01-09 11:11:37 +0900

    Added gpu.c

---
+++ gpu.c
@@ -0,0 +1,76 @@
+#include <cublas.h>
+#include <stdio.h>
+
+#define IDX2C(i,j,ld) (((j)*(ld))+(i))
+#define IDX3C(c,i,j,in_h,in_w) ((c)*((in_h)*(in_w)) + (i)*(in_w) +(j))
+#define IDX4C(n,c,i,j,in_c,in_h,in_w) ((n)*((in_c)*(in_h)*(in_w)) + (c)*((in_h)*(in_w)) + (i)*(in_w) +(j))
+#define IDX5C(t,n,c,i,j,in_n,in_c,in_h,in_w) ((t)*((in_n)*(in_c)*(in_h)*(in_w)) + (n)*((in_c)*(in_h)*(in_w)) + (c)*((in_h)*(in_w)) + (i)*(in_w) +(j))
+#define BREAK return(enif_make_int(env, 0));
+#define PI 3.14159265358979323846
+#define SIGMOID(x)  (1 / (1+exp(-1*x)))
+#define CHECK(call)                                   \
+{                                                     \
+    const cudaError_t error = call;                   \
+    if (error != cudaSuccess)                         \
+    {                                                 \
+        return enif_make_int(env,10000+(int)error);   \
+    }                                                 \
+}
+#define CUBLAS(call)                                  \
+{                                                     \
+    const cublasStatus error = call;                  \
+    if (error != CUBLAS_STATUS_SUCCESS)               \
+    {                                                 \
+        return(11000+(int)error);   \
+    }                                                 \
+}
+
+
+int mult1(int a_bin, int b_bin){
+    int  c_bin;
+    int r1, c1, r2, c2, n, i, j;
+    float *a,*b,*c;
+    float* devPtrA;
+    float* devPtrB;
+    float* devPtrC;
+
+    n = r1*c2;
+    //a = (float *) a_bin.data;
+    //b = (float *) b_bin.data;
+    //c = (float *) enif_make_new_binary(env, n * sizeof(float), &c_bin);
+
+    for(j=0;j<c2;j++)
+        for(i=0;i<r1;i++)
+            c[IDX2C(i,j,r1)] = 0.0;
+
+
+    // Initialize CUBLAS
+    cublasInit();
+
+    CUBLAS(cublasAlloc (r1*c1, sizeof(*a), (void**)&devPtrA));
+    CUBLAS(cublasAlloc (r2*c2, sizeof(*b), (void**)&devPtrB));
+    CUBLAS(cublasAlloc (r1*c2, sizeof(*c), (void**)&devPtrC));
+
+    CUBLAS(cublasSetMatrix (r1, c1, sizeof(*a), a, r1, devPtrA, r1));
+    CUBLAS(cublasSetMatrix (r2, c2, sizeof(*b), b, r2, devPtrB, r2));
+    CUBLAS(cublasSetMatrix (r1, c2, sizeof(*c), c, r1, devPtrC, r1));
+
+
+    //Sgemm
+    cublasSgemm('N', 'N', r1, c2, c1, 1.0, devPtrA, r1, devPtrB, r2, 0.0, devPtrC, r1);
+
+
+    CUBLAS(cublasGetMatrix (r1, c2, sizeof(*c), devPtrC, r1, c, r1));
+ 
+    // Shutdown CUBLAS
+    cublasFree(devPtrA);
+    cublasFree(devPtrB);
+    cublasFree(devPtrC);
+    cublasShutdown();
+    
+
+    return(c_bin);
+
+}
+
+

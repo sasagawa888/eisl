980363a3e6ac1fa3df8e7be89428ffbedb297c4d
sasagawa888 <ken-s@tkcnf.or.jp>
2021-04-01 13:01:48 +0900

    Updated version number

--- eisl.h
+++ eisl.h
@@ -18,7 +18,7 @@ Copying GC mode
 
 #include <setjmp.h>
 #include <stdbool.h>
-#define VERSION     1.85
+#define VERSION     1.86
 #define HEAPSIZE    20000000
 #define CELLSIZE    20000000
 #define WORK1        6000000

3fb7de4af6b34819b4526c33dd8c05e854f678b6
sasagawa888 <ken-s@tkcnf.or.jp>
2021-06-25 08:29:10 +0900

    Fixed syntax-highlight

--- syn_highlight.c
+++ syn_highlight.c
@@ -14,7 +14,7 @@ static const char *special[] = {
 static const char *syntax[] = {
     "lambda", "labels", "flet", "let", "let*", "setq", "setf",
     "defconstant", "defun", "defmacro", "defglobal", "defdynamic",
-    "dynamic", "function", "function*", "symbol-function", "class",
+    "dynamic", "set-dynamic", "function", "function*", "symbol-function", "class",
     "and", "or", "if", "cond", "while", "for", "block", "return-from",
     "case", "case-using", "progn", "defclass", "defgeneric", "defgeneric*",
     "defmethod", "dynamic-let", "ignore-errors", "catch", "throw",

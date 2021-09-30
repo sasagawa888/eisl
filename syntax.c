37f77cf977cef2a1bbbd2b7dde072d67a5905993
sasagawa888 <ken-s@tkcnf.or.jp>
2021-10-01 07:51:51 +0900

    Comment out setf fixed code. It causes ILOS

--- syntax.c
+++ syntax.c
@@ -474,11 +474,12 @@ f_setf(int arglist)
     // e.g. (setf (access-foo-a x) 100) 
     else if (listp(arg1) && length(arg1) == 2) {
 	// a method returns it's variable name 
-    if(functionp(car(arg1))){
+    //comment out fix code. it causes error veirfy/object.lsp
+    //if(functionp(car(arg1))){
 	var = eval(list2(car(arg1), NIL));
-    }
-    else
-    error (IMPROPER_ARGS,"setf",arg1);
+    //}
+    //else
+    //error (IMPROPER_ARGS,"setf",arg1);
 
 	newform =
 	    cons(makesym("SET-SLOT-VALUE"),

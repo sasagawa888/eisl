d810aa6d7dbe45b600a8bcb825c62553bbc185b2
sasagawa888 <ken-s@tkcnf.or.jp>
2021-10-31 18:45:37 +0900

    Fixed next-method-p  error-check

--- function.c
+++ function.c
@@ -4893,11 +4893,11 @@ f_next_method_p(int arglist)
 {
     int             method;
 
-    if (length(arglist) != 0)
-	error(UNDEF_FUN, "next-method-p", arglist);
     if (generic_func == NIL)
 	error(UNDEF_FUN, "next-method-p", NIL);
-
+    if (length(arglist) != 0)
+	error(WRONG_ARGS, "next-method-p", arglist);
+    
     method = cdr(next_method);
     while (!nullp(method)) {
 	int             varlist;

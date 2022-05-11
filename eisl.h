daf96d1ad3a1aa7426475ea857ca1018dc9f3650
sasagawa888 <ken-s@tkcnf.or.jp>
2022-05-12 08:07:25 +0900

    Update version number

--- eisl.h
+++ eisl.h
@@ -75,7 +75,7 @@
 
 
 
-static const float VERSION = 2.40;
+static const float VERSION = 2.41;
 static const int WORK1 = ((int)((CELLSIZE) / 20 * 6));
 static const int WORK2 = ((int)((CELLSIZE) / 20 * 13));
 static const int FREESIZE = 900;

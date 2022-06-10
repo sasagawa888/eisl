25f04f020357c214a137c35d0bfc561595c9ecba
sasagawa888 <ken-s@tkcnf.or.jp>
2022-06-10 18:47:39 +0900

    Remove FFT

--- eisl.h
+++ eisl.h
@@ -242,10 +242,7 @@ extern int      bigcell[BIGSIZE];
 // bignum pointer, FFT_input_vector
 extern int big_pt0;
 extern int big_pt1;
-extern double complex fftx[FFTSIZE];
-extern double complex ffty[FFTSIZE];
-extern double complex fftz[FFTSIZE];
-extern int ffti[FFTSIZE];
+
 
 #define DEF_GETTER(RETURN_TYPE, NAME, MEMBER, DEFAULT) \
     static inline RETURN_TYPE GET_ ## NAME (int addr)  \

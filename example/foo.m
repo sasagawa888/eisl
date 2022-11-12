d936be727b3718a059734b7cfdae898fe48cda3a
sasagawa888 <ken-s@tkcnf.or.jp>
2022-11-12 15:15:24 +0900

    Modify example/foo.m meta-expression sample

--- example/foo.m
+++ example/foo.m
@@ -3,9 +3,9 @@
 foo[x] <= x+1
 
 fact[n] <= 
-    [eq[n;0]->1;t->*[n;fact[n-1]]]
+    [=[n;0]->1;t->times[n;fact[n-1]]]
 
 fib[n] <=
-    [eq[n;0]->0;
-     eq[n;1]->1;
+    [=[n;0]->0;
+     =[n;1]->1;
      t->plus[fib[n-1];fib[n-2]]]

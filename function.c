54c11d1d98e80c6ed2ba632d6714c25d86d11003
sasagawa888 <ken-s@tkcnf.or.jp>
2023-06-13 08:02:29 +0900

    Fixed create-string. adapt unicode char.

--- function.c
+++ function.c
@@ -4085,7 +4085,7 @@ int check_dimension(int ls)
 
 int f_create_string(int arglist)
 {
-    int arg1, arg2, n;
+    int arg1, arg2, n, i, pos;
     char *str, c;
 
     arg1 = car(arglist);
@@ -4108,12 +4108,70 @@ int f_create_string(int arglist)
     else
 	c = GET_CHAR(arg2);
 
+    if(isUni1(c)){
     str = ALLOC(n + 1);
     memset(str, c, n);
     str[n] = NUL;
+    }
+    else if(isUni2(c)){
+    str = ALLOC(2*n + 1);
+    pos = 0;
+    for(i=0;i<n;i++){
+    str[pos++] = heap[arg1].name[0];
+    str[pos++] = heap[arg1].name[1];
+    }
+    str[pos] = NUL;    
+    }
+    else if(isUni3(c)){
+    str = ALLOC(3*n + 1);
+    pos = 0;
+    for(i=0;i<n;i++){
+    str[pos++] = heap[arg2].name[0];
+    str[pos++] = heap[arg2].name[1];
+    str[pos++] = heap[arg2].name[2];
+    }
+    str[pos] = NUL;     
+    }
+    else if(isUni4(c)){
+    str = ALLOC(4*n + 1);
+    pos = 0;
+    for(i=0;i<n;i++){
+    str[pos++] = heap[arg2].name[0];
+    str[pos++] = heap[arg2].name[1];
+    str[pos++] = heap[arg2].name[2];
+    str[pos++] = heap[arg2].name[3];
+    }
+    str[pos] = NUL;     
+    }
+    else if(isUni5(c)){
+    str = ALLOC(5*n + 1);
+    pos = 0;
+    for(i=0;i<n;i++){
+    str[pos++] = heap[arg2].name[0];
+    str[pos++] = heap[arg2].name[1];
+    str[pos++] = heap[arg2].name[2];
+    str[pos++] = heap[arg2].name[3];
+    str[pos++] = heap[arg2].name[4];
+    }
+    str[pos] = NUL;     
+    }
+    else if(isUni6(c)){
+    str = ALLOC(6*n + 1);
+    pos = 0;
+    for(i=0;i<n;i++){
+    str[pos++] = heap[arg2].name[0];
+    str[pos++] = heap[arg2].name[1];
+    str[pos++] = heap[arg2].name[2];
+    str[pos++] = heap[arg2].name[3];
+    str[pos++] = heap[arg2].name[4];
+    str[pos++] = heap[arg2].name[5];
+    }
+    str[pos] = NUL;     
+    }
     int res = make_str(str);
     FREE(str);
     return res;
+
 }
 
 int f_parse_number(int arglist)

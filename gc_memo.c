24fe575c548ca22adfe239115621c7b4c08bdfe0
sasagawa888 <ken-s@tkcnf.or.jp>
2023-07-25 08:47:11 +0900

    idea memo for thread pooling

--- gc_memo.c
+++ gc_memo.c
@@ -40,3 +40,74 @@ void exit_concurrent(void){
     pthread_mutex_unlock(&mutex);
 }
 
+//---thread pooling-----------------------------------------------
+int queue[10];
+int queue_pt;
+int input[10];
+int output[10];
+struct para d[10];
+pthread_t para_thread[10]
+pthread_cond_t cond_para[10];
+
+void enqueue(int n){
+    queue[queue_pt] = n;
+    queue_pt++;
+}
+
+void dequeue(int arg1){
+    int th,i;
+
+    th = queue[0];
+    for(i=0;i<queue_pt;i++){
+        queue[i] = queue[i+1];
+    }
+    pthread_mutex_lock(&mutex);
+    d[th].in = arg1;
+    d[th].num = th;
+    pthread_cond_signal(&cond_para[th]);
+    pthread_mutex_unlock(&mutex);
+}
+
+struct para {
+    int in;
+    int num;
+    int out;
+};
+
+
+void *parallel(void *arg);
+void *parallel(void *arg)
+{
+    struct para *pd = (struct para *) arg;
+    while(1){
+    pthread_mutex_lock(&mutex);
+    pthread_cond_wait(&cond_para[pd->num], &mutex); 
+    pthread_mutex_unlock(&mutex);
+    if(parallel_exit_flag)
+        goto exit;
+    output[pd->num] = eval(input[pd->num], pd->num);
+    }
+    exit:
+    pthread_exit(NULL);
+}
+
+
+void init_para(void){
+    int i;
+
+    for(i=0;i<10;i++){
+        pthread_create(para_thread[i],NULL,parallel,&d[i]);
+        queue[i] = i+1;
+    }
+    queue_pt = 0;
+}
+
+void exit_para(void){
+    int i;
+    pthread_mutex_lock(&mutex);
+    parallel_exit_flag = 1;
+    for(i=0,i<10;i++){
+        pthread_cond_signal(&cond_para[i]);
+    } 
+    pthread_mutex_unlock(&mutex);
+}

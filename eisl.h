c491ae3b741391ac5af0641ecd3306c9dd8d31c0
sasagawa888 <ken-s@tkcnf.or.jp>
2023-08-04 18:01:27 +0900

    Debugging parallel

--- eisl.h
+++ eisl.h
@@ -81,7 +81,7 @@
 
 
 static const float VERSION = 3.32;
-static const int FREESIZE = 5000000;
+static const int FREESIZE = 900000;
 static const int SYMSIZE = 256;
 static const int CHARSIZE = 7;	// unicode char. add \0 to tail
 static const int MATSIZE = 256;

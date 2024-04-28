c5134eaa992883ffb7811e87b69feb209f16cad1
sasagawa888 <ken-s@tkcnf.or.jp>
2024-04-28 16:49:39 +0900

    Making mp-call compiler

--- tests/bug0.c
+++
@@ -1,2 +0,0 @@
-#include "fast.h"
-static int f_TARAIstar(int arglist, int th);static int TARAIstar(int X,int Y,int Z, int th);
--- tests/bug1.c
+++
@@ -1,7 +0,0 @@
-static int f_TARAIstar(int arglist, int th){
-int arg1,arg2,arg3;
-arg1 = Fnth(0,arglist);
-arg2 = Fnth(1,arglist);
-arg3 = Fnth(2,arglist);
-return(TARAIstar(arg1,arg2,arg3,th));
-}

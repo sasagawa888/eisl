3ebf8df71f9b84b411c600fd4d3c42792b5a147c
sasagawa888 <ken-s@tkcnf.or.jp>
2024-06-13 17:14:04 +0900

    Update version number.

--- eisl.h
+++ eisl.h
@@ -87,7 +87,7 @@ extern int check_sw;
 
 
 
-static const float VERSION = 3.99;
+static const float VERSION = 4.00;
 static const int FREESIZE = 3000000;
 static const int SYMSIZE = 256;
 static const int CHARSIZE = 7;	// unicode char. add \0 to tail

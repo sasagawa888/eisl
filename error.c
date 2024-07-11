ee8eba9fe99bc5ffba4916dfb5c2ef0e7e729aa4
sasagawa888 <ken-s@tkcnf.or.jp>
2024-07-11 13:11:55 +0900

    Improve error-handling in network-mode

--- error.c
+++ error.c
@@ -661,6 +661,22 @@ int signal_condition(int x, int y, int th)
 	handling_resource_err = false;
 	signal_condition_x = x;
 	signal_condition_y = y;
+	str = cdr(assoc(make_sym("a"), GET_CDR(x)));
+    args = cdr(assoc(make_sym("b"), GET_CDR(x)));
+    fun = cdr(assoc(make_sym("c"), GET_CDR(x)));
+    output_stream = error_stream;
+    if (repl_flag)
+	ESCERRFRED();
+    fputs(GET_NAME(str), stderr);
+    print(fun);
+    fputc(' ', stderr);
+    print(args);
+    fputc('\n', stderr);
+    if (repl_flag)
+	ESCERRFORG();
+    input_stream = standard_input;
+    output_stream = standard_output;
+    handling_resource_err = false;
 	send_to_parent(make_sym("\x15"));
 	RAISE(Exit_Network);
     }

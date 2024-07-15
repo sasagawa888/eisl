0f63f6df1659b97ff72ed9f2c06b63022e314abd
sasagawa888 <ken-s@tkcnf.or.jp>
2024-07-15 18:44:42 +0900

    Remove dynamic_var in fast.h

--- fast.h
+++ fast.h
@@ -28,7 +28,7 @@ static fn6 f6[NUM_FN6S];
 static fn7 f7[NUM_FN7S];
 static fn8 f8[NUM_FN8S];
 volatile sig_atomic_t exit_flag = 0;
-static int dynamic_var[256][20];
+
 
 void signal_handler_c(int signo)
 {

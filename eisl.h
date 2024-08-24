f4f04f9314bd173e04b5a468270fc05b07d45741
sasagawa888 <ken-s@tkcnf.or.jp>
2024-08-24 11:04:37 +0900

    Update version number

--- eisl.h
+++ eisl.h
@@ -89,7 +89,7 @@
 
 
 
-static const float VERSION = 5.26;
+static const float VERSION = 5.30;
 static const int FREESIZE = 1000000;
 static const int SYMSIZE = 256;
 static const int CHARSIZE = 7;	// unicode char. add \0 to tail

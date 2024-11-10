43edc2f232b8599b0acd0c33d72e6052f23b5d82
sasagawa888 <ken-s@tkcnf.or.jp>
2024-11-10 12:26:23 +0900

    Add comment for copy_instance.

--- data.c
+++ data.c
@@ -967,6 +967,26 @@ int instancep(int x)
 		return(NIL);
 }
 
+
+/*
+*  defclass eval :initform val. 
+*  when create make new instance, copy instance of val.
+*  e.g.
+(defclass <inner> () ((slot-inner :accessor slot-inner :initarg slot-inner)))
+(defclass <outer> () ((slot-outer :reader slot-outer :initform (create (class <inner>) 'slot-inner 'hoge))))
+(let ((outer-1 (create (class <outer>)))
+      (outer-2 (create (class <outer>))))
+  (format (standard-output) "~S~%" (slot-inner (slot-outer outer-1)))
+  ;; => HOGE
+  (format (standard-output) "~S~%" (slot-inner (slot-outer outer-2)))
+  ;; => HOGE
+  (setf (slot-inner (slot-outer outer-1)) 'FUGA)
+  (format (standard-output) "~S~%" (slot-inner (slot-outer outer-1)))
+  ;; => FUGA
+  (format (standard-output) "~S~%" (slot-inner (slot-outer outer-2)))
+  ;; => Expected HOGE, but FUGA
+  )
+*/
 int copy_instance(int x)
 {
     int addr, initls, cl;

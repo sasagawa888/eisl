a5f6ffb45110ffe51c3b093b1f40f8f7272f0837
sasagawa888 <ken-s@tkcnf.or.jp>
2025-11-16 09:02:56 +0900

    fix -m option

--- main.c
+++ main.c
@@ -335,7 +335,7 @@ static void usage(void)
 	 "-f           -- EISL starts after reading formatter.lsp.\n"
 	 "-h           -- display help.\n"
 	 "-l filename  -- EISL starts after reading the file.\n"
-	 "-m N(mega)   -- EISL starts with N*1000000(>=10mega) cells.\n"
+	 "-m N         -- EISL starts with N*1000000(>=10mega) cells.\n"
 	 "-n           -- EISL starts as network child mode.\n"
 	 "-r           -- EISL does not use editable REPL.\n"
 	 "-s filename  -- EISL runs the file with script mode.\n"

83036490c7981fa5e0a18bac02eb6d77c8eb6f10
sasagawa888 <ken-s@tkcnf.or.jp>
2025-11-24 16:16:14 +0900

    update edlis version

--- edlis.h
+++ edlis.h
@@ -9,7 +9,7 @@
 #define FULLSCREEN
 #include "term.h"
 
-static const float VERSION = 5.50;
+static const float VERSION = 5.60;
 
 #define ROW_SIZE 5000
 #define COL_SIZE 256
--- gbc.c
+++ gbc.c
@@ -95,7 +95,7 @@ void gbc_sequence(void)
     /* mark generic_list */
     mark_cell(generic_list);
 
-    /* mard setf_list. generic functions related setf */
+    /* mark setf_list. generic functions related setf */
     mark_cell(setf_list);
 
 
